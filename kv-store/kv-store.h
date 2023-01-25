ac64a7bfbbea6d50acdb07f91f23be7dd2b09173
Tales Scopinho <36113553+talicopanda@users.noreply.github.com>
2023-01-24 19:08:16 -0500

    init: CMake starter code

---
+++ kv-store/kv-store.h
@@ -0,0 +1,8 @@
+﻿// kv-store.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
