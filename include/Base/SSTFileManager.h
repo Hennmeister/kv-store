d9ce204dd551cafbcaab82098a38d219d7c4610f
VijayS02 <Vijaysambamurthy@gmail.com>
2023-03-19 10:52:06 -0400

    Update SortedSSTManager to use SSTFileManager

--- include/Base/SSTFileManager.h
+++ include/Base/SSTFileManager.h
@@ -1,11 +1,13 @@
 #ifndef KV_STORE_SSTFILEMANAGER_H
 #define KV_STORE_SSTFILEMANAGER_H
 #include <vector>
+using namespace std;
 class SSTFileManager {
 public:
-    virtual int get_page(int page, int file, void* data_buf) =0;
-    virtual int scan(int start_page, int end_page, int file, void* data_buf) =0;
-    virtual int write_file(void* data, int size, int &new_file_num)=0;
+    virtual int get_page(int page, string filename, void* data_buf) =0;
+    virtual int scan(int start_page, int end_page, string filename, void* data_buf) =0;
+    virtual int write_file(void* data, int size, string filename)=0;
+    virtual vector<pair<string, int>> get_files()=0;
 };
 
 #endif //KV_STORE_SSTFILEMANAGER_H
