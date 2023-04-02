de508e64c73d86566dd14dbed8cfbcb7ea762597
VijayS02 <Vijaysambamurthy@gmail.com>
2023-04-02 11:47:27 -0400

    Add override modifier for methods in SimpleSSTFileManager

--- include/SimpleSSTFileManager.h
+++ include/SimpleSSTFileManager.h
@@ -12,10 +12,10 @@ private:
 public:
     explicit SimpleSSTFileManager(string target_dir);
     ~SimpleSSTFileManager();
-    int get_page(int page, string filename, void* data_buf);
-    int scan(int start_page, int end_page, string filename, void* data_buf);
-    int write_file(void* data, int size, string filename, void* metadata);
-    vector<pair<string, int>> get_files();
+    int get_page(int page, string filename, void* data_buf) override;
+    int scan(int start_page, int end_page, string filename, void* data_buf) override;
+    int write_file(void* data, int size, string filename, void* metadata) override;
+    vector<pair<string, int>> get_files() override;
     int get_metadata(void* data, string filename) override;
 };
 
